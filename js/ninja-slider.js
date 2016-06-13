b176f0a66f7406676466d758a90cf072f3c3ae21
Matt Pill <matt@hellofutu.re>
2016-06-13 19:54:24 +0100

    Amendments to styling

--- js/ninja-slider.js
+++ js/ninja-slider.js
@@ -4,8 +4,8 @@ var nsOptions =
     sliderId: "ninja-slider",
     transitionType: "zoom", //"fade", "slide", "zoom", "kenburns 1.2" or "none"
     autoAdvance: true,
-    delay: 5000,
-    transitionSpeed: 3000,
+    delay: 8000,
+    transitionSpeed: 8000,
     aspectRatio: "2:1",
     initSliderByCallingInitFunc: false,
     shuffle: false,
